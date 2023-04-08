8235420727397a5cee5332ba216a507e6f261927
Folareen <sakawahab03@gmail.com>
2023-04-08 20:09:10 +0100

    done with products page

--- frontend/pages/products.js
+++ frontend/pages/products.js
@@ -1,11 +1,21 @@
 import { Box, CircularProgress, Typography } from "@mui/material";
+import { urlFor } from "../client";
 import ProductCard from "../components/ProductCard";
 import useFetch from "../hooks/useFetch";
 import formatImageUrl from "../utils/formatImageUrl";
 
-const Products = ({ baseUrl }) => {
+const Products = () => {
   const { data, loading, error } = useFetch(
-    `${baseUrl}/api/categories?populate[products][populate][0]=images`
+    `*[_type == 'product']{
+    title,
+    description,
+    price,
+    images,
+    productId,
+    category->{
+      title,
+    }
+  }`, []
   );
 
   if (loading) {
@@ -24,68 +34,73 @@ const Products = ({ baseUrl }) => {
     );
   }
 
+
   if (error) {
     return (
       <Typography sx={{ color: "danger.main" }}>Error Occurred...</Typography>
     );
   }
 
+
+  const laptops = data?.filter(product => product.category.title == 'laptops')
+  const phones = data?.filter(product => product.category.title == 'phones')
+  const accessories = data?.filter(product => product.category.title == 'accessories')
+
   return (
     <Box>
-      {data.map(({ attributes }) => {
-        return (
-          <Box key={attributes.name}>
-            <Typography
-              sx={{
-                fontSize: 20,
-                mt: 2,
-                textTransform: "capitalize",
-                color: "dark.main",
-                textAlign: { xs: "left", md: "center" },
-                mx: { md: 2, xs: 1.5 },
-              }}
-            >
-              {attributes.name}
-            </Typography>
-            <Box
-              sx={{
-                display: "flex",
-                overflowX: "auto",
-                width: "100%",
-                justifyContent: { md: "center" },
-              }}
-            >
-              {attributes.products.data.map(
-                ({ id, attributes: { title, price, images } }) => {
-                  return (
-                    <ProductCard
-                      img_url={formatImageUrl(
-                        baseUrl,
-                        images.data[0].attributes.url
-                      )}
-                      title={title}
-                      price={price}
-                      productId={id}
-                      categoryId={attributes.name}
-                      key={id}
-                    />
-                  );
-                }
-              )}
-            </Box>
-          </Box>
-        );
-      })}
+      <Box key={'laptops'}>
+        <Typography sx={{ fontSize: 20, mt: 2, textTransform: 'capitalize', color: 'dark.main', textAlign: { xs: 'left', md: 'center' }, mx: { md: 2, xs: 1.5 } }}>
+          laptops
+        </Typography>
+        <Box sx={{ display: 'flex', overflowX: 'auto', width: '100%', justifyContent: { md: 'center' } }}>
+          {
+            laptops.map(
+              ({ productId, title, price, images }) => {
+                return <ProductCard
+                  img_url={urlFor(images[0].asset).url()}
+                  title={title} price={price} productId={productId?.current} categoryId={'laptops'} key={productId?.current} />
+              }
+            )
+          }
+        </Box>
+      </Box>
+
+      <Box key={'phones'}>
+        <Typography sx={{ fontSize: 20, mt: 2, textTransform: 'capitalize', color: 'dark.main', textAlign: { xs: 'left', md: 'center' }, mx: { md: 2, xs: 1.5 } }}>
+          phones
+        </Typography>
+        <Box sx={{ display: 'flex', overflowX: 'auto', width: '100%', justifyContent: { md: 'center' } }}>
+          {
+            phones.map(
+              ({ productId, title, price, images }) => {
+                return <ProductCard
+                  img_url={urlFor(images[0].asset).url()}
+                  title={title} price={price} productId={productId?.current} categoryId={'phones'} key={productId?.current} />
+              }
+            )
+          }
+        </Box>
+      </Box>
+
+      <Box key={'accessories'}>
+        <Typography sx={{ fontSize: 20, mt: 2, textTransform: 'capitalize', color: 'dark.main', textAlign: { xs: 'left', md: 'center' }, mx: { md: 2, xs: 1.5 } }}>
+          accessories
+        </Typography>
+        <Box sx={{ display: 'flex', overflowX: 'auto', width: '100%', justifyContent: { md: 'center' } }}>
+          {
+            accessories.map(
+              ({ productId, title, price, images }) => {
+                return <ProductCard
+                  img_url={urlFor(images[0].asset).url()}
+                  title={title} price={price} productId={productId?.current} categoryId={'accessories'} key={productId} />
+              }
+            )
+          }
+        </Box>
+      </Box>
+
     </Box>
   );
 };
 
 export default Products;
-
-export const getServerSideProps = async () => {
-  return {
-    props: {
-      baseUrl: process.env.BASE_URL,
-    },
-  };
-};
