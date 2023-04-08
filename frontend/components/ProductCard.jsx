478b06923efcaee9dd76bf72d71dd3daec164e07
Folareen <sakawahab03@gmail.com>
2023-04-08 18:26:20 +0100

    fixed hydration error

--- frontend/components/ProductCard.jsx
+++ frontend/components/ProductCard.jsx
@@ -23,8 +23,7 @@ const ProductCard = ({ img_url, title, price, productId, categoryId }) => {
 
     return (
         <Link href={`/${categoryId}/${productId}`}>
-            <a >
-                <Box sx={{ minWidth: { xs: '150px', sm: '220px' }, maxWidth: { xs: '150px', sm: '220px' }, m: { sm: 2, xs: 1.5 }, boxShadow: '0 0 4px rgba(0, 0, 0, 0.3)', borderRadius: 4, bgcolor: 'light.main', transition: 'all 1s ease', ...boxHoverStyle, borderStyle: 'solid', borderColor: 'secondary.main', borderWidth: '0.5px' }} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} >
+                <Box  sx={{ minWidth: { xs: '150px', sm: '220px' }, maxWidth: { xs: '150px', sm: '220px' }, m: { sm: 2, xs: 1.5 }, boxShadow: '0 0 4px rgba(0, 0, 0, 0.3)', borderRadius: 4, bgcolor: 'light.main', transition: 'all 1s ease', ...boxHoverStyle, borderStyle: 'solid', borderColor: 'secondary.main', borderWidth: '0.5px' }} onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} >
 
                     <Box sx={{ height: { xs: '120px', sm: '150px', lg: '200px' }, width: '100%', mb: 1.5, borderRadius: 4, borderBottomLeftRadius: 0, borderBottomRightRadius: 0, borderStyle: 'solid', borderColor: 'secondary.main', borderWidth: 0, borderBottomWidth: '0.5px', transition: 'all 1s ease', opacity: 0.8, ...imageHoverStyle }}>
                         <img src={img_url} style={{ width: '100%', height: '100%', objectFit: 'cover', borderTopRightRadius: 'inherit', borderTopLeftRadius: 'inherit' }} alt={`picture of ${title}`} />
@@ -39,7 +38,6 @@ const ProductCard = ({ img_url, title, price, productId, categoryId }) => {
                     </Typography>
 
                 </Box>
-            </a>
         </Link>
 
     )
