ea60b16fc4109a323fc7e25eb4e0f540e77140e0
Folareen <sakawahab03@gmail.com>
2023-04-08 16:54:37 +0100

    done

--- frontend/components/TrendingProducts.jsx
+++ frontend/components/TrendingProducts.jsx
@@ -1,40 +1,73 @@
 import { Box, Typography } from "@mui/material"
+import { urlFor } from "../client"
 import formatImageUrl from "../utils/formatImageUrl"
 import ProductCard from "./ProductCard"
 
-const TrendingProducts = ({trendingProducts, baseUrl}) => {
-  return (
-    <Box sx={{ mt: 5}}>
+const TrendingProducts = ({ trendingProducts }) => {
+    const laptops = trendingProducts.filter(product => product.category.title == 'laptops')
+    const phones = trendingProducts.filter(product => product.category.title == 'phones')
+    const accessories = trendingProducts.filter(product => product.category.title == 'accessories')
 
-        <Typography sx={{fontSize: 26, color: 'dark.main', textAlign: 'center'}}>
-            Trending Gadgets
-        </Typography>
+    return (
+        <Box sx={{ mt: 5 }}>
 
-        {
-            trendingProducts.map(
-                ({attributes}) => {
-                    return(
-                        <Box key={attributes.name}>
-                            <Typography sx={{fontSize: 20, mt: 2, textTransform: 'capitalize', color: 'dark.main', textAlign: {xs: 'left', md: 'center'}, mx:{md: 2, xs: 1.5} }}>
-                                {attributes.name}
-                            </Typography>
-                            <Box sx={{display: 'flex', overflowX: 'auto', width: '100%', justifyContent: {md: 'center'}}}>
-                            {
-                                attributes.products.data.slice(0, 3).map(
-                                    ({id, attributes: {title, price, images }}) => {
-                                        return <ProductCard img_url={formatImageUrl(baseUrl, images.data[0].attributes.url)} title={title} price={price}  productId={id} categoryId={attributes.name} key={id} />
-                                    }
-                                )
+            <Typography sx={{ fontSize: 26, color: 'dark.main', textAlign: 'center' }}>
+                Trending Gadgets
+            </Typography>
+
+            <Box key={'laptops'}>
+                <Typography sx={{ fontSize: 20, mt: 2, textTransform: 'capitalize', color: 'dark.main', textAlign: { xs: 'left', md: 'center' }, mx: { md: 2, xs: 1.5 } }}>
+                    laptops
+                </Typography>
+                <Box sx={{ display: 'flex', overflowX: 'auto', width: '100%', justifyContent: { md: 'center' } }}>
+                    {
+                        laptops.slice(0, 3).map(
+                            ({ productId, title, price, images }) => {
+                                return <ProductCard 
+                                img_url={urlFor(images[0].asset).url()} 
+                                title={title} price={price} productId={productId.current} categoryId={'laptops'} key={productId?.current} />
                             }
-                            </Box>
-                        </Box>
-                    )
-                }
-            )
-        }
+                        )
+                    }
+                </Box>
+            </Box>
 
-    </Box>
-  )
+            <Box key={'phones'}>
+                <Typography sx={{ fontSize: 20, mt: 2, textTransform: 'capitalize', color: 'dark.main', textAlign: { xs: 'left', md: 'center' }, mx: { md: 2, xs: 1.5 } }}>
+                    phones
+                </Typography>
+                <Box sx={{ display: 'flex', overflowX: 'auto', width: '100%', justifyContent: { md: 'center' } }}>
+                    {
+                        phones.slice(0, 3).map(
+                            ({ productId, title, price, images }) => {
+                                return <ProductCard 
+                                img_url={urlFor(images[0].asset).url()} 
+                                title={title} price={price} productId={productId?.current} categoryId={'phones'} key={productId?.current} />
+                            }
+                        )
+                    }
+                </Box>
+            </Box>
+
+            <Box key={'accessories'}>
+                <Typography sx={{ fontSize: 20, mt: 2, textTransform: 'capitalize', color: 'dark.main', textAlign: { xs: 'left', md: 'center' }, mx: { md: 2, xs: 1.5 } }}>
+                    accessories
+                </Typography>
+                <Box sx={{ display: 'flex', overflowX: 'auto', width: '100%', justifyContent: { md: 'center' } }}>
+                    {
+                        accessories.slice(0, 3).map(
+                            ({ productId, title, price, images }) => {
+                                return <ProductCard 
+                                img_url={urlFor(images[0].asset).url()} 
+                                title={title} price={price} productId={productId?.current} categoryId={'accessories'} key={productId} />
+                            }
+                        )
+                    }
+                </Box>
+            </Box>
+
+        </Box>
+    )
 }
 
 export default TrendingProducts
